4af4e4af600ccb30c9e65fd328f2ca71d3033ed4
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2022-02-15 18:15:28 -0500

    Update MainUI.hx

--- src/omega/engine/hub/window/MainUI.hx
+++ src/omega/engine/hub/window/MainUI.hx
@@ -3,27 +3,21 @@ package omega.engine.hub.window;
 import flixel.FlxState;
 
 class MainUI extends FlxState {
-	
-  	public var elapsed:Int = 0;
-	
+	public var elapsed:Int = 0;
+
 	// Header
 	public var title:FlxText;
-  
+
 	override public function create() {
-		
 		super.create();
-		
+
 		title = new FlxText(0, 0, 0, "Omega").setFormat("monsterrat", 24);
 		title.screenCenter(X);
 		add(title);
-		
 	}
 
 	override public function update(dt:Float) {
-		
 		super.update(dt);
-    		elapsed += dt;
-		
+		elapsed += dt;
 	}
-	
 }
