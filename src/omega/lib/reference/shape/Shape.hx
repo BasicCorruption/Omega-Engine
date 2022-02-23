5636fc58cce098eb98f82acb877ad4498534c30d
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-02-23 14:27:26 -0500

    Update Shape.hx

--- src/omega/lib/reference/shape/Shape.hx
+++ src/omega/lib/reference/shape/Shape.hx
@@ -1,5 +1,11 @@
 package omega.lib.reference.shape;
 
 interface Shape {
+  public var width:Int;
+  public var height:Int;
   
+  public var image:FlxGraphicAsset;
+  public var color:FlxColor;
+  
+  public function new();
 }
