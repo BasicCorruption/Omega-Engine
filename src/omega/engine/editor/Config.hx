92ca85dbae8f31d72e9cf6f5ebbaf90df0f08019
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-02-23 11:38:02 -0500

    Update Config.hx

--- src/omega/engine/editor/Config.hx
+++ src/omega/engine/editor/Config.hx
@@ -1,5 +1,10 @@
 package omega.engine.editor;
 
 class Config {
-  public static var window = {};
+  public static var window = {
+    width: 960,
+    height: 540,
+    drawspeed: 60,
+    updatespeed: 60
+  };
 }
